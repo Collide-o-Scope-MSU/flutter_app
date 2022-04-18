f4f7f767a2aa3a498392a400dfbfebd512c22e56
ajaya.dahal <ajayadahal1000@gmail.com>
2022-04-18 04:44:28 -0500

    Colors added in UI

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -24,6 +24,7 @@ class _FirebaseGetDataState extends State<FirebaseGetData> {
   FirebaseDatabase database = FirebaseDatabase.instance;
   DatabaseReference ref = FirebaseDatabase.instance.ref();
   DatabaseReference starCountRef = FirebaseDatabase.instance.ref('/');
+  List<Color> my_colors = [Colors.red, Colors.grey];
 
   final List<UserModel>? growableList = <UserModel>[];
 
@@ -83,7 +84,7 @@ class _FirebaseGetDataState extends State<FirebaseGetData> {
               //print(data);
               //List<Map> lists;
               Map<String, dynamic> map = json.decode(data.toString());
-
+              growableList?.clear();
               final length = map.length;
               //print(map["Cellular1"]);
               for (int i = 1; i < length; i++) {
@@ -94,20 +95,20 @@ class _FirebaseGetDataState extends State<FirebaseGetData> {
                   UserModel user = UserModel.fromJson(map["Cellular$i"]);
                   user.type = "Cellular";
                   //print(user);
-                  if (growableList?.indexOf(user) == -1) {
-                    growableList?.add(user);
-                    count++;
-                  }
+                  //if (growableList?.indexOf(user) == -1) {
+                  growableList?.add(user);
+                  count++;
+                  //}
                 }
                 if (map["LoRA$i"] != null) {
                   print(map["LoRA$i"]);
                   UserModel user = UserModel.fromJson(map["LoRA$i"]);
                   user.type = "LoRa";
                   //print(user);
-                  if (growableList?.indexOf(user) == -1) {
-                    growableList?.add(user);
-                    count++;
-                  }
+                  //if (growableList?.indexOf(user) == -1) {
+                  growableList?.add(user);
+                  count++;
+                  //}
                 }
               }
 
@@ -123,6 +124,7 @@ class _FirebaseGetDataState extends State<FirebaseGetData> {
               //List tagObjs =
               //  tagObjsJson.map((user) => UserModel.fromJson(user)).toList();
               //print(user);
+              Color this_color = my_colors[0];
 
               return ListView.builder(
                 itemCount: length,
@@ -137,15 +139,29 @@ class _FirebaseGetDataState extends State<FirebaseGetData> {
                       physics: NeverScrollableScrollPhysics(),
                       children: [
                         ListTile(
-                          title: Text(
-                            'ID: ${growableList?.elementAt(i).id}  Voltage: ${growableList?.elementAt(i).battery}',
-                            textAlign: TextAlign.center,
+                          title: Transform.translate(
+                            offset: Offset(-9, 0),
+                            child: Text(
+                              'ID: ${growableList?.elementAt(i).id}  Voltage: ${growableList?.elementAt(i).battery}',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: getColor(
+                                      growableList?.elementAt(i).status),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
                           ),
                           subtitle: Text(
-                              'GPS: ${growableList?.elementAt(i).GPS}\nLatest_Update: ${growableList?.elementAt(i).time}\nStatus: ${growableList?.elementAt(i).status}'),
+                            'GPS: ${growableList?.elementAt(i).GPS}\nLatest_Update: ${growableList?.elementAt(i).time}\nStatus: ${growableList?.elementAt(i).status}',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: getColor(
+                                    growableList?.elementAt(i).status)),
+                          ),
                           leading: Image.asset(
                               'assets/images/${growableList?.elementAt(i).type}.jpg'),
-                          trailing: const Icon(Icons.battery_std),
+                          trailing: Icon(getBatteryIcon(
+                              growableList?.elementAt(i).battery)),
                           onTap: () {
                             final gps =
                                 growableList?.elementAt(i).GPS?.split(',');
@@ -166,6 +182,22 @@ class _FirebaseGetDataState extends State<FirebaseGetData> {
             }
           },
         ));
+  }
+
+  IconData getBatteryIcon(voltage) {
+    if (double.parse(voltage) < 2) {
+      return Icons.battery_alert;
+    } else if (double.parse(voltage) < 3) {
+      return Icons.battery_saver;
+    } else
+      return Icons.battery_full;
+  }
+
+  Color getColor(status) {
+    if (status == "Normal")
+      return Colors.white;
+    else
+      return Color.fromARGB(255, 253, 17, 0);
   }
 
   static Future<void> openMap(double latitude, double longitude) async {
